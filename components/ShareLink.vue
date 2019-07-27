a7fd8c331915d1e32fc61d7de7771132d96ab801
宇山 拓夢 <sasurai.usagi3@gmail.com>
2019-07-28 07:50:14 +0900

    update: シェアURLデザイン変更

--- components/ShareLink.vue
+++ components/ShareLink.vue
@@ -1,12 +1,15 @@
 <template>
   <div>
-    <template v-if="accessKey != null">
-      <div>共有URL: <input type="text" readonly :value="`https://nippohub.com/daily_reports/public/${accessKey}`" style="width: 100%"></div>
-      <button type="button" class="p-submit" @click="dismissAccessKey">プライベートにする</button>
-    </template>
+    <div class="p-share-link" v-if="accessKey != null">
+      <span>共有URL:&nbsp;</span>
+      <input type="text" readonly class="p-share-link__url p-oneline-input" :value="`https://nippohub.com/daily_reports/public/${accessKey}`">
+      <button type="button" class="p-share-link__disable-btn p-submit" @click="dismissAccessKey">プライベートにする</button>
+    </div>
     <button type="button" class="p-submit" v-else @click="generateAccessKey">共有する</button>
   </div>
 </template>
 
 <script src="~/assets/javascripts/components/share_link.js"></script>
+<style src="~/assets/stylesheets/share_link.css"></style>
+<style src="~/assets/stylesheets/oneline_input.css"></style>
 <style src="~/assets/stylesheets/submit.css"></style>
