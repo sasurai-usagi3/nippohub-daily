45ce323864de4b76f0383851ff6f17bfb949d869
宇山 拓夢 <sasurai.usagi3@gmail.com>
2019-08-25 09:25:27 +0900

    add: UserRepository#fetch

--- assets/javascripts/repositories/user_repository.js
+++ assets/javascripts/repositories/user_repository.js
@@ -1,6 +1,15 @@
 import firebase from '~/assets/javascripts/util/firebase';
+import User from '~/assets/javascripts/models/user';
 
 export default class UserRepository {
+  fetch() {
+    return new Promise((resolve, reject) => {
+      firebase.auth().onAuthStateChanged(user => {
+        resolve(user != null ? new User(user.uid, user.email) : null);
+      });
+    });
+  }
+
   create(email, password) {
     return firebase.auth().createUserWithEmailAndPassword(email, password);
   }
