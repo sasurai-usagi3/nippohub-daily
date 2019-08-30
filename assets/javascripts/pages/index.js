0c975a49784505b663b1b71a44e756b6150c3abf
宇山 拓夢 <sasurai.usagi3@gmail.com>
2019-08-30 09:52:55 +0900

    clean: pages直

--- assets/javascripts/pages/index.js
+++ assets/javascripts/pages/index.js
@@ -1,12 +1,36 @@
-import DailyReportIndex from '~/components/DailyReportIndex.vue';
+import MainHeader from '~/components/MainHeader.vue';
+import DailyReportForm from '~/components/DailyReportForm.vue';
+import DailyReportList from '~/components/DailyReportList.vue';
+import DailyReportListLink from '~/components/DailyReportListLink.vue';
+import DailyReportSearchForm from '~/components/DailyReportSearchForm.vue';
+import MainFooter from '~/components/MainFooter.vue';
 import UserRepository from '~/assets/javascripts/repositories/user_repository';
 
 export default {
-  data: function() {
-    return {currentUser: null}
-  },
   components: {
-    DailyReportIndex
+    DailyReportForm,
+    DailyReportList,
+    DailyReportListLink,
+    DailyReportSearchForm,
+    MainHeader,
+    MainFooter
+  },
+  data: function() {
+    const today = new Date();
+    const firstDate = new Date(today.getFullYear(), today.getMonth(), 1, 0, 0, 0, 0);
+    const endDate = new Date(today.getFullYear(), today.getMonth() + 1, 0, 0, 0, 0, 0);
+
+    return {currentUser: null, firstDate, endDate}
+  },
+  watch: {
+    '$route': function() {
+      const today = new Date();
+      const firstDateFromParams = this.$route.query.start_at;
+      const endDateFromParams = this.$route.query.end_at;
+
+      this.firstDate = firstDateFromParams != null && firstDateFromParams !== '' ? new Date(Date.parse(firstDateFromParams)) : new Date(today.getFullYear(), today.getMonth(), 1, 0, 0, 0, 0);
+      this.endDate = endDateFromParams != null && endDateFromParams !== '' ? new Date(Date.parse(endDateFromParams)) : new Date(today.getFullYear(), today.getMonth() + 1, 0, 0, 0, 0, 0);
+    }
   },
   mounted: function() {
     const repository = new UserRepository();
